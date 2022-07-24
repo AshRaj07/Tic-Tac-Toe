252646a56b8799521b8531a601d67647bd01fdcd
Aishwary Rajput <aish.raj2000.a@gmail.com>
2022-07-25 00:16:35 +0530

    Added Cross and Zero display logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Square from './Square'
 
 const Board = () => {
+    const [board, setboard] = useState(Array(9).fill(null));
+    const [isSetX, setisSetX] = useState(false)
+    const handleSquareClick = (pos) => {
+        if(board[pos]!=null){
+            return;
+        }
+        setboard((prevState)=>{
+            return prevState.map((val,idx)=>{
+                if(idx==pos){
+                    return isSetX ? "X" : "O";
+                }
+                return val;
+            })
+        })
+        setisSetX(!isSetX);
+    }
+
+    const renderSquare = (pos) =>{
+        return  <Square value={board[pos]} onClick={()=>{handleSquareClick(pos)}} />
+    }
   return (
     <>
     <div className='board'>
         <div className='board-row'>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
         </div>
         <div className='board-row'>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
         </div>
         <div className='board-row'>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
         </div>
 
     </div>
